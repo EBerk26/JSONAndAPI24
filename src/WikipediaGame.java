168ded6f7c70c06319a350764c78cf4c57465cbe
EBerk26 <eli_berk26@milton.edu>
2024-12-19 09:10:20 -0500

    class 12/19 - minor changes

--- src/WikipediaGame.java
+++ src/WikipediaGame.java
@@ -9,6 +9,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class WikipediaGame {
     public static void main(String[] args) {
@@ -21,28 +22,31 @@ public class WikipediaGame {
 }
 class WikipediaPage{
     String title;
-    ArrayList<WikipediaPage> parents;
-    ArrayList<WikipediaPage> children;
+    ArrayList<WikipediaPage> parents = new ArrayList<>();
+    ArrayList<WikipediaPage> children = new ArrayList<>();
     boolean parentsFound = false;
     boolean childrenFound=false;
     JSONObject json;
     WikipediaPage(String title){
+        System.out.println(title);
         String titleUsingUnderscores = title;
         //replace spaces with underscores
         while(titleUsingUnderscores.contains(" ")){
             int indexOfSpace = titleUsingUnderscores.indexOf(" ");
             titleUsingUnderscores=titleUsingUnderscores.substring(0,indexOfSpace)+"_"+titleUsingUnderscores.substring(indexOfSpace+1,titleUsingUnderscores.length());
         }
-        System.out.println(titleUsingUnderscores);
         json = importJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=links%7Clinkshere&list=&titles="+titleUsingUnderscores+"+&formatversion=2&pllimit=max&lhprop=title%7Credirect%7Cpageid&lhlimit=max");
-        System.out.println();
     }
     void findParents(){
         JSONObject query = (JSONObject) json.get("query");
         JSONArray pages = (JSONArray) query.get("pages");
         JSONObject content = (JSONObject) pages.get(0);
         JSONArray links = (JSONArray) content.get("linkshere");
-        System.out.println();
+        for(int x =0;x<links.size();x++){
+            JSONObject objectInArray = (JSONObject)links.get(x);
+            parents.add(new WikipediaPage((String)objectInArray.get("title")));
+        }
+
         //query (obj)- value (obj) - pages (array) - value (array) -
 
         parentsFound = true;
@@ -51,6 +55,9 @@ class WikipediaPage{
 
     }
     JSONObject importJSON(String link){
+        if(link.contains("Pablo")){
+            System.out.println();
+        }
         String output;
         StringBuilder jsonString= new StringBuilder();
         try {
@@ -60,11 +67,6 @@ class WikipediaPage{
             conn.setRequestMethod("GET");
             conn.setRequestProperty("Accept", "application/json");
 
-            if (conn.getResponseCode() != 200) {
-
-                throw new RuntimeException("Failed : HTTP error code : "
-                        + conn.getResponseCode());
-            }
 
             BufferedReader br = new BufferedReader(new InputStreamReader(
                     (conn.getInputStream())));
